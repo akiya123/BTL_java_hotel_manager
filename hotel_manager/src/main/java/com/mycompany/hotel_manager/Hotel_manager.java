2800427700dfe682325d59633be33fcb6eebb136
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2024-11-23 14:45:54 +0700

    Create Hotel_manager.java

---
+++ hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotel_manager;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Hotel_manager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
