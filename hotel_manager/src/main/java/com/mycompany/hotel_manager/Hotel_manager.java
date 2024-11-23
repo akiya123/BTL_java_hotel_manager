1dc2df640134d5db666bcde568e430c740da3d7d
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2024-11-23 16:26:21 +0700

    Update Hotel_manager.java

--- hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
+++ hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
@@ -1,16 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.hotel_manager;
+import Custommer_login_interface;
+import java.awt.*;
+import javax.swing.*;
 
-/**
- *
- * @author Administrator
- */
 public class Hotel_manager {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worl!");
+        Custommer_login_interface p = new Custommer_login_interface();
+        p.setVisible(true);
     }
 }
