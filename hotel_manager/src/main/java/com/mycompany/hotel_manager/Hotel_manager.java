aef8649ae476edb8d384e30df32b9e908f1be71e
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2024-11-23 15:21:25 +0700

    Update Hotel_manager.java

--- hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
+++ hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
@@ -11,6 +11,6 @@ package com.mycompany.hotel_manager;
 public class Hotel_manager {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worl!");
     }
 }
