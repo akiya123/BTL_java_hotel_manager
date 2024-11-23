3b172535cbb7af9fdba46df698ae4b3250a73677
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2024-11-23 21:14:44 +0700

    Update Hotel_manager.java

--- hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
+++ hotel_manager/src/main/java/com/mycompany/hotel_manager/Hotel_manager.java
@@ -1,13 +1,13 @@
 
 package com.mycompany.hotel_manager;
 
-import com.mycompany.hotel_manager.Custommer_interface.login;
+import com.mycompany.hotel_manager.Custommer_interface.Log_in;
 
 
 public class Hotel_manager {
 
     public static void main(String[] args) {
-        login p = new login();
+        Log_in p = new Log_in();
         p.setVisible(true);
         
     }
