2da208c6eee63b94c6c212632f299936c19e2606
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2024-12-13 21:28:28 +0700

    Update ManagerMenu.java
    Chỉ là add code vào thôi
    - Nếu lỗi ông chỉ việc xóa phần từ //Hoàn đến // Hoàn - end
    -Nhữn thứ từ //Hoàn đến //Hoàn - end là của panel mới
    - Đã back up nên cứ pull

--- src/view/second_sreen/Manager/ManagerMenu.java
+++ src/view/second_sreen/Manager/ManagerMenu.java
@@ -12,6 +12,67 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 
 public class ManagerMenu extends javax.swing.JFrame {
+
+// Hoàn
+   //Biến có sự để tâm thấp
+    private javax.swing.JPanel panelManageUser;//Panel chính
+    private javax.swing.JScrollPane jScrollPane1; // Tạo đinh dạng cho bảng
+
+    private javax.swing.JLabel lbPhoneManagerUser; // Chữ "SĐT"
+    private javax.swing.JLabel lbCustommerNameManagerUser;// Chữ "Tên"
+    private javax.swing.JLabel lbTitleManagerUser; // Chữ "Thông tin khách hàng"
+    
+    //Biến cần để tâm
+        // Những biến khởi tạo sẽ ẩn đi - Hiện khi nhấn sự kiện ButtonFindManageUser
+                //Mail
+                private javax.swing.JLabel lbSetMailManageUser;
+                private javax.swing.JLabel lbMailManageUserFlase;
+
+        /*Để trả nhận ý tưởng là tạo 1 biến để kiểm tra trạng thái nhận phòng của phòng
+         tạo 1 ArrayList<pair<"Tên phòng","Một biến boollen">> - arrBooking lưu trạng thái nhận trả phòng
+         nếu True = đã nhận phòng || false = đã trả phòng -> xóa phòng khỏi bảng
+         lấy dữ liệu của list từ database
+         - tìm trên database xem Username nào gắn với phòng nào để hiện vào bảng
+         - Username từ txtCustommerNameManageUser
+         */
+
+        private javax.swing.JButton ButtonCheckInManageUserFlase; // Nút nhận phòng - sẽ chọn phòng check in tại bảng
+        private javax.swing.JButton ButtonCheckOutManageUserFlase; // Nút Trả phòng
+        private javax.swing.JButton ButtonClearManageUserFalse; /*Nút làm mới -
+        ý tưởng dùng để ẩn tất cả biến có False ở cuối tên và setText txtCustommerNameManageUser và txtCustommerPhoneManageUser */ 
+
+        private javax.swing.JButton ButtonDateInExpiredManageUserFlase; /*  Nút phòng hết hạn - 
+        ý tưởng là nếu hết hạn nhận phòng thì sẽ để trạng thái phòng là phòng trống rồi xóa phòng khỏi bảng */
+
+        //Thông tin đặt phòng - ý tưởng là khi nhấn vào hàng nào thông tin của hàng đấy sẽ hiện trên những biến lbSet
+
+        private javax.swing.JLabel lbBookingInformationManageUserFlase; // Chữ "thông tin đặt phòng"
+
+        private javax.swing.JTable tableBookingInformationManageUserFalse;// bảng Chứa thông tin
+
+                private javax.swing.JLabel lbRoomInformationManageUserFalse; // Chữ "thông tin phòng"
+                //Label để hiện chữ
+                
+                private javax.swing.JLabel lbRoomNameManagerUserFalse;
+                private javax.swing.JLabel lbRoomTypeManagerUserFalse;
+                private javax.swing.JLabel lbDateInManagerUserFalse;
+                private javax.swing.JLabel lbDateOutManagerUserFalse;
+                //Label để hiện thông tin
+                private javax.swing.JLabel lbSetDateInManagerUserFalse;
+                private javax.swing.JLabel lbSetDateOutManagerUserFalse;
+                private javax.swing.JLabel lbSetRoomNameManagerUserFalse;
+                private javax.swing.JLabel lbSetRoomTypeManagerUserFalse;
+
+    
+        //Những biến sẽ hiện
+        private javax.swing.JButton buttonFindManageUser; // Nút tìm
+        
+                //txt nhập thông tin tìm
+                private javax.swing.JTextField txtCustommerNameManageUser; // tên
+                private javax.swing.JTextField txtCustommerPhoneManageUser; // SĐT
+
+
+//Hoàn - end
     private ManagerService managerService;
     private UserService userService;
 
@@ -36,16 +97,18 @@ public class ManagerMenu extends javax.swing.JFrame {
 
     private javax.swing.JTextField txtDateCustommerDetail;
 
+
     // KhuongPham EDIT
     private DefaultTableModel tableModel1;
     private DefaultTableModel tableModel2;
 
 
     public ManagerMenu() {
+        initComponents();
+        setLocationRelativeTo(null);
         managerService = new ManagerService();
         userService = new UserService();
-        setLocationRelativeTo(null);
-        initComponents();
+
 
         butonLogOut.addActionListener(this::logOutEvent);
         buttonReportCustommerDetail.addActionListener(this::reportEvent);
@@ -96,6 +159,34 @@ public class ManagerMenu extends javax.swing.JFrame {
         butonLogOut = new javax.swing.JButton();
         jLabel10 = new javax.swing.JLabel();
 
+        //Hoàn
+        panelManageUser = new javax.swing.JPanel();
+        lbPhoneManagerUser = new javax.swing.JLabel();
+        lbTitleManagerUser = new javax.swing.JLabel();
+        lbCustommerNameManagerUser = new javax.swing.JLabel();
+        txtCustommerPhoneManageUser = new javax.swing.JTextField();
+        txtCustommerNameManageUser = new javax.swing.JTextField();
+        buttonFindManageUser = new javax.swing.JButton();
+        lbMailManageUserFlase = new javax.swing.JLabel();
+        lbSetMailManageUser = new javax.swing.JLabel();
+        ButtonCheckOutManageUserFlase = new javax.swing.JButton();
+        ButtonClearManageUserFalse = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tableBookingInformationManageUserFalse = new javax.swing.JTable();
+        lbBookingInformationManageUserFlase = new javax.swing.JLabel();
+        ButtonCheckInManageUserFlase = new javax.swing.JButton();
+        lbRoomInformationManageUserFalse = new javax.swing.JLabel();
+        lbDateInManagerUserFalse = new javax.swing.JLabel();
+        lbRoomNameManagerUserFalse = new javax.swing.JLabel();
+        lbRoomTypeManagerUserFalse = new javax.swing.JLabel();
+        lbDateOutManagerUserFalse = new javax.swing.JLabel();
+        ButtonDateInExpiredManageUserFlase = new javax.swing.JButton();
+        lbSetDateOutManagerUserFalse = new javax.swing.JLabel();
+        lbSetRoomNameManagerUserFalse = new javax.swing.JLabel();
+        lbSetRoomTypeManagerUserFalse = new javax.swing.JLabel();
+        lbSetDateInManagerUserFalse = new javax.swing.JLabel();
+        //Hoàn - end
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Hotel management system");
 
@@ -119,7 +210,7 @@ public class ManagerMenu extends javax.swing.JFrame {
         tableListManageRoom.setModel(tableModel1);
         tableListManageRoom.setAutoCreateRowSorter(true);
 
-        managerService.loadDataFromDatabaseForManager(tableModel1);
+        managerService.loadDataFromDatabaseForManager(tableModel1); 
 
         jScrollPane2.setViewportView(tableListManageRoom);
         if (tableListManageRoom.getColumnModel().getColumnCount() > 0) {
@@ -287,6 +378,152 @@ public class ManagerMenu extends javax.swing.JFrame {
         );
 
         jTabbedPane1.addTab("Custommer detail bill", jPanel4);
+
+        // Hoàn
+        // thêm tab mới ở đây
+        panelManageUser.setBackground(new java.awt.Color(204, 255, 255));
+        panelManageUser.setPreferredSize(new java.awt.Dimension(952, 540));
+        panelManageUser.setLayout(null);
+
+        lbPhoneManagerUser.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        lbPhoneManagerUser.setText("SĐT:");
+        panelManageUser.add(lbPhoneManagerUser);
+        lbPhoneManagerUser.setBounds(101, 119, 48, 32);
+
+        lbTitleManagerUser.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        lbTitleManagerUser.setText("Thông tin khách hàng");
+        panelManageUser.add(lbTitleManagerUser);
+        lbTitleManagerUser.setBounds(33, 19, 235, 32);
+
+        lbCustommerNameManagerUser.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        lbCustommerNameManagerUser.setText("Tên:");
+        panelManageUser.add(lbCustommerNameManagerUser);
+        lbCustommerNameManagerUser.setBounds(105, 79, 45, 32);
+        panelManageUser.add(txtCustommerPhoneManageUser);
+        txtCustommerPhoneManageUser.setBounds(167, 125, 190, 30);
+        panelManageUser.add(txtCustommerNameManageUser);
+        txtCustommerNameManageUser.setBounds(167, 77, 190, 30);
+
+        buttonFindManageUser.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        buttonFindManageUser.setText("Tìm");
+        panelManageUser.add(buttonFindManageUser);
+        buttonFindManageUser.setBounds(413, 77, 127, 29);
+
+        lbMailManageUserFlase.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        lbMailManageUserFlase.setText("mail:");
+        panelManageUser.add(lbMailManageUserFlase);
+        lbMailManageUserFlase.setBounds(110, 170, 50, 32);
+        panelManageUser.add(lbSetMailManageUser);
+        lbSetMailManageUser.setBounds(170, 170, 190, 32);
+
+        ButtonCheckOutManageUserFlase.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        ButtonCheckOutManageUserFlase.setText("Trả phòng");
+        panelManageUser.add(ButtonCheckOutManageUserFlase);
+        ButtonCheckOutManageUserFlase.setBounds(550, 130, 127, 29);
+
+        ButtonClearManageUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        ButtonClearManageUserFalse.setText("Làm mới");
+        panelManageUser.add(ButtonClearManageUserFalse);
+        ButtonClearManageUserFalse.setBounds(550, 80, 127, 27);
+
+        //Thông tin bảng đặt phòng
+        tableBookingInformationManageUserFalse.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                
+            },
+            new String [] {
+                "STT", "Tên phòng", "Loại phòng", "Trạng thái"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tableBookingInformationManageUserFalse);
+        if (tableBookingInformationManageUserFalse.getColumnModel().getColumnCount() > 0) {
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(0).setMinWidth(50);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(0).setPreferredWidth(50);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(0).setMaxWidth(50);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(1).setMinWidth(125);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(1).setPreferredWidth(125);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(1).setMaxWidth(125);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(2).setMinWidth(125);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(2).setPreferredWidth(125);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(2).setMaxWidth(125);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(3).setMinWidth(100);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(3).setPreferredWidth(100);
+            tableBookingInformationManageUserFalse.getColumnModel().getColumn(3).setMaxWidth(100);
+        }
+
+        panelManageUser.add(jScrollPane1);
+        jScrollPane1.setBounds(90, 260, 400, 272);
+
+        lbBookingInformationManageUserFlase.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbBookingInformationManageUserFlase.setText("Thông tin đặt phòng");
+        panelManageUser.add(lbBookingInformationManageUserFlase);
+        lbBookingInformationManageUserFlase.setBounds(90, 220, 200, 32);
+
+        ButtonCheckInManageUserFlase.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        ButtonCheckInManageUserFlase.setText("Nhận phòng");
+        panelManageUser.add(ButtonCheckInManageUserFlase);
+        ButtonCheckInManageUserFlase.setBounds(410, 130, 127, 29);
+
+        lbRoomInformationManageUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbRoomInformationManageUserFalse.setText("Thông tin phòng");
+        panelManageUser.add(lbRoomInformationManageUserFalse);
+        lbRoomInformationManageUserFalse.setBounds(660, 220, 160, 30);
+
+        lbDateInManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbDateInManagerUserFalse.setText("Thời gian nhận:");
+        panelManageUser.add(lbDateInManagerUserFalse);
+        lbDateInManagerUserFalse.setBounds(530, 380, 130, 30);
+
+        lbRoomNameManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbRoomNameManagerUserFalse.setText("Tên:");
+        panelManageUser.add(lbRoomNameManagerUserFalse);
+        lbRoomNameManagerUserFalse.setBounds(620, 280, 40, 30);
+
+        lbRoomTypeManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbRoomTypeManagerUserFalse.setText("Loại :");
+        panelManageUser.add(lbRoomTypeManagerUserFalse);
+        lbRoomTypeManagerUserFalse.setBounds(610, 330, 50, 30);
+
+        lbDateOutManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lbDateOutManagerUserFalse.setText("Thời gian trả:");
+        panelManageUser.add(lbDateOutManagerUserFalse);
+        lbDateOutManagerUserFalse.setBounds(550, 430, 110, 30);
+
+        ButtonDateInExpiredManageUserFlase.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        ButtonDateInExpiredManageUserFlase.setText("Phòng quá hạn Nhận");
+        panelManageUser.add(ButtonDateInExpiredManageUserFlase);
+        ButtonDateInExpiredManageUserFlase.setBounds(690, 130, 180, 29);
+
+        lbSetDateOutManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lbSetDateOutManagerUserFalse.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        panelManageUser.add(lbSetDateOutManagerUserFalse);
+        lbSetDateOutManagerUserFalse.setBounds(670, 430, 190, 30);
+
+        lbSetRoomNameManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lbSetRoomNameManagerUserFalse.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        panelManageUser.add(lbSetRoomNameManagerUserFalse);
+        lbSetRoomNameManagerUserFalse.setBounds(670, 280, 190, 30);
+
+        lbSetRoomTypeManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lbSetRoomTypeManagerUserFalse.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        panelManageUser.add(lbSetRoomTypeManagerUserFalse);
+        lbSetRoomTypeManagerUserFalse.setBounds(670, 330, 190, 30);
+
+        lbSetDateInManagerUserFalse.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lbSetDateInManagerUserFalse.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        panelManageUser.add(lbSetDateInManagerUserFalse);
+        lbSetDateInManagerUserFalse.setBounds(670, 380, 190, 30);
+
+        jTabbedPane1.addTab("Manger user", panelManageUser);
+        //Hoàn - end
 
         jPanel5.setBackground(new java.awt.Color(204, 255, 255));
 
